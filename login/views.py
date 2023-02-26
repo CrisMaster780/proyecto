b715d002ef5a583bdb50a7c5944b5f1582a5aa48
CrisMaster780 <95488614+CrisMaster780@users.noreply.github.com>
2023-02-26 20:05:05 -0300

    commit_4

--- login/views.py
+++ login/views.py
@@ -6,13 +6,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.sessions.backends.db import SessionStore
 from padron.models import Sucursal
 import json
+from padron.models import Padron
 
 
 def index(request):
     sucursal = Sucursal.objects.all()
     if request.method == 'GET':
         sucursal = Sucursal.objects.all()
-
         return render(request, 'Login/index.html', {
             'sucursal': sucursal
         })
@@ -21,35 +21,27 @@ def index(request):
         _usu = request.POST['username']
         _pass = request.POST['password']
         user = authenticate(request, username=_usu, password=_pass)
-
         if user is None:
             return render(request, 'Login/index.html', {
                 'error': 'USUARIO O CONTRASENA INCORRECTOS',
                 'sucursal': sucursal
             })
-
         else:
-            
             login(request, user)
-            id_usu = request.user.id
-            #estr = Sucursal.objects.filter(usuario__id__contains= id_usu).first()
-            
-
-            #print(estr)
-           # request.session[0] = estr
             return redirect('/principal')
 
 
 def principal(request):
     usuario = request.user.username
-    #sucursal = request.session['0']
     id_usu = request.user.id
-    estr = Sucursal.objects.filter(usuario__id__contains= id_usu).first()
+    estr = Sucursal.objects.filter(usuario__id__contains=id_usu).first()
     titulo = 'Principal'
-    return render(request, 'Login/principal.html', {
+    padron = Padron.objects.all()
+    return render(request, 'Padron/index.html', {
         'usuario': usuario,
         'titulo': titulo,
-        'sucursal': estr
+        'sucursal': estr,
+        'lista': padron
     })
 
 
